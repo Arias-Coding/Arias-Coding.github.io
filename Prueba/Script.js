8773e9bc1ed5657613dd88060c9e1bbb16e25a42
Arias Coding <87912468+Arias-Coding@users.noreply.github.com>
2022-08-09 12:46:25 -0300

    Update Script.js

--- Prueba/Script.js
+++ Prueba/Script.js
@@ -1,56 +1,76 @@
+class Comentario {
+    constructor(comentario) {
+        this.id = Math.trunc(Math.random()*1000)
+        this.comentario = comentario
+        this.respuestas = new Array()
+    }
+    responder(respuesta) {
+        this.respuestas.push(respuesta)
+    }
+}
+listaDeComentarios = []
+
 let boton = document.getElementById("boton")
 let lista = document.getElementById("lista")
 let place = document.getElementById("texto")
         
-boton.addEventListener("click", ()=>{
-    let p = document.createElement("P")
+        
+const agregarRespuesta = (place, lista) =>{
+    
+    let nuevoComentario = new Comentario(place.value)
+    listaDeComentarios.push(nuevoComentario)
+    
+    //
     let respuesta = document.createElement("DIV")
-    let texto = document.createTextNode(place.value)
+    let p = document.createElement("P")
+    let texto = document.createTextNode(nuevoComentario.comentario)
     p.appendChild(texto)
+    p.classList.add("aca")
+    
     respuesta.appendChild(p)
-    place.value = null
-
-//   
-    let id = Math.trunc(Math.random()*1000)
-
+    
     let divV = document.createElement("DIV")
     divV.classList.add("listaRespuestas")
+    
     let input = document.createElement("INPUT")
     let buttom = document.createElement("BUTTON")
-    let textoo = document.createTextNode("Responder")
+    
     respuesta.appendChild(divV)
     respuesta.appendChild(input)
     respuesta.appendChild(buttom)
+    
+    let textoo = document.createTextNode("Responder")
     buttom.appendChild(textoo)
+    buttom.setAttribute('id', nuevoComentario.id)
+    input.setAttribute('id', `id${nuevoComentario.id}`)
+    divV.setAttribute('id', `id${nuevoComentario.id}`)
+    
     buttom.classList.add("responder")
     input.classList.add("respuesta")
-    respuesta.classList.add(id)
     respuesta.classList.add("id")
-
-    lista.appendChild(respuesta)
-    console.log(respuesta)
-// 
     
-    let responderr = document.querySelectorAll(".responder")    
-    responderr.forEach((key)=>{
-        key.addEventListener("click", ()=>{
-            responder()
-        })
+    lista.appendChild(respuesta)
+    
+    // 
+    place.value = null
+    //
+    buttom.addEventListener("click", () => {
+        agregarRespuesta( input.value, divV)
+        input.value = null
     })
+}
+        
+        
+        
+boton.addEventListener("click", ()=>{
+    agregarRespuesta(place, lista)
 })
 
 
-let responder = ()=> {
-    const respuestaa = document.querySelector(".respuesta")
-    let respuesta = respuestaa.value
-
-    let listaRespuestas = document.querySelector(".listaRespuestas")
-    
+let responder = (id, respuesta, lista)=> {
     let p = document.createElement("P")
     let texto = document.createTextNode(respuesta)
     p.appendChild(texto)
-    listaRespuestas.appendChild(p)
-    respuestaa.value = null
-
-    console.log(respuesta)
+    
+    lista.appendChild(p)
 }
