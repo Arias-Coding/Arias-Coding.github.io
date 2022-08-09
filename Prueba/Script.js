0193be510ef134607d8028e599149b849da3019c
Arias Coding <87912468+Arias-Coding@users.noreply.github.com>
2022-08-08 23:41:57 -0300

    Add files via upload

--- Prueba/Script.js
+++ Prueba/Script.js
@@ -3,14 +3,33 @@ let lista = document.getElementById("lista")
 let place = document.getElementById("texto")
         
 boton.addEventListener("click", ()=>{
-    let li = document.createElement("P")
+    let p = document.createElement("P")
+    let respuesta = document.createElement("DIV")
     let texto = document.createTextNode(place.value)
-    li.appendChild(texto)
-    lista.appendChild(li)
+    p.appendChild(texto)
+    respuesta.appendChild(p)
     place.value = null
 
-    comentar()
+//   
+    let id = Math.trunc(Math.random()*1000)
 
+    let divV = document.createElement("DIV")
+    divV.classList.add("listaRespuestas")
+    let input = document.createElement("INPUT")
+    let buttom = document.createElement("BUTTON")
+    let textoo = document.createTextNode("Responder")
+    respuesta.appendChild(divV)
+    respuesta.appendChild(input)
+    respuesta.appendChild(buttom)
+    buttom.appendChild(textoo)
+    buttom.classList.add("responder")
+    input.classList.add("respuesta")
+    respuesta.classList.add(id)
+    respuesta.classList.add("id")
+
+    lista.appendChild(respuesta)
+    console.log(respuesta)
+// 
     
     let responderr = document.querySelectorAll(".responder")    
     responderr.forEach((key)=>{
@@ -19,28 +38,6 @@ boton.addEventListener("click", ()=>{
         })
     })
 })
-
-let comentar = ()=> {
-    let id = Math.trunc(Math.random()*1000)
-    let div = document.createElement("DIV")
-    div.classList.add("respuestas")
-    div.classList.add(id)
-
-    let divV = document.createElement("DIV")
-    divV.classList.add("listaRespuestas")
-    let input = document.createElement("INPUT")
-    let buttom = document.createElement("BUTTON")
-    let texto = document.createTextNode("Responder")
-    div.appendChild(divV)
-    div.appendChild(input)
-    div.appendChild(buttom)
-    buttom.appendChild(texto)
-    buttom.classList.add("responder")
-    input.classList.add("respuesta")
-
-    lista.appendChild(div)
-    console.log(div)
-}
 
 
 let responder = ()=> {
