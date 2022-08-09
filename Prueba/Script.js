3464b099c70e6bc427e9213594d55bca0a673adb
Arias Coding <87912468+Arias-Coding@users.noreply.github.com>
2022-08-08 22:57:29 -0300

    Add files via upload

--- Prueba/Script.js
+++ Prueba/Script.js
@@ -8,4 +8,52 @@ boton.addEventListener("click", ()=>{
     li.appendChild(texto)
     lista.appendChild(li)
     place.value = null
+
+    comentar()
+
+    
+    let responderr = document.querySelectorAll(".responder")    
+    responderr.forEach((key)=>{
+        key.addEventListener("click", ()=>{
+            responder()
+        })
+    })
 })
+
+let comentar = ()=> {
+    let id = Math.trunc(Math.random()*1000)
+    let div = document.createElement("DIV")
+    div.classList.add("respuestas")
+    div.classList.add(id)
+
+    let divV = document.createElement("DIV")
+    divV.classList.add("listaRespuestas")
+    let input = document.createElement("INPUT")
+    let buttom = document.createElement("BUTTON")
+    let texto = document.createTextNode("Responder")
+    div.appendChild(divV)
+    div.appendChild(input)
+    div.appendChild(buttom)
+    buttom.appendChild(texto)
+    buttom.classList.add("responder")
+    input.classList.add("respuesta")
+
+    lista.appendChild(div)
+    console.log(div)
+}
+
+
+let responder = ()=> {
+    const respuestaa = document.querySelector(".respuesta")
+    let respuesta = respuestaa.value
+
+    let listaRespuestas = document.querySelector(".listaRespuestas")
+    
+    let p = document.createElement("P")
+    let texto = document.createTextNode(respuesta)
+    p.appendChild(texto)
+    listaRespuestas.appendChild(p)
+    respuestaa.value = null
+
+    console.log(respuesta)
+}
